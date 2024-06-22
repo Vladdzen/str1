3d9023cd9e4b5214faf2de50a687aea273ca4b0d
Vladdzen <vladislavklimakov@yandex.ru>
2024-06-22 17:03:16 +0300

    Add files via upload

---
+++ str1.py
@@ -0,0 +1,5 @@
+Number_of_completed_remote_sensing = 12
+Number_of_hours_spent = 1.5
+CourseName = 'Python'
+Time_for_one_task = Number_of_hours_spent / Number_of_completed_remote_sensing
+print('Курс:',CourseName,',', 'всего задач:',Number_of_completed_remote_sensing,',', 'затрачено часов:', Number_of_hours_spent,',','среднее время выполнения',Time_for_one_task,'часа.')
